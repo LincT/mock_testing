b48183db8af9e76ae8a8d5a9da5258052f107776
Linc <test@testserver.com>
2018-02-13 15:30:34 -0600

    git ignore update

--- test/test_main.py
+++ test/test_main.py
@@ -9,8 +9,24 @@ class TestMain(TestCase):
     def test_main(self, mock_print, mock_random):
         main.main()
         print_calls = mock_print.call_args_list
-
         self.assertIn('win', str(print_calls))
+
+    @patch('main.main.random_num', return_value=10)
+    @patch('builtins.print')
+    def test_mock_print_count(self, mock_print, mock_random):
+        main.main()
+        self.assertEqual(2,mock_print.call_count)
+
+    @patch('main.main.random_num', return_value=10)
+    @patch('builtins.print')
+    def test_mock_print_contains(self, mock_print, mock_random):
+        main.main()
+        mock_print.assert_called_with('win')
+
+    # tried adding this from what was described, may need to return to this
+    @patch('builtins.input',return_value="some text")
+    def test_print(self, mock_input):
+        self.assertEqual(mock_input, "some_text")
 
 
 if __name__ == '__main__':
